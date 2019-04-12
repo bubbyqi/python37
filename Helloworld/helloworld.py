070aff62e3c40525ef8e816285ca498f7951318b
齐冠权 <qiguanquan@ebyf.com.cn>
2019-04-12 13:48:39 +0800

    Initial commit

---
+++ Helloworld/helloworld.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     helloworld
+   Description :
+   Author :       Administrator
+   date：          2019/4/12 13:47
+-------------------------------------------------
+   Change Activity:
+                   13:47:
+-------------------------------------------------
+"""
+__author__ = 'Administrator'
+print('hello python')
